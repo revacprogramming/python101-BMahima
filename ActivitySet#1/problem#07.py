49786327bd42bdda7a9863422f22d13bf9506f07
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-16 10:28:46 +0000

    problem 7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,6 @@
 # Strings
-
 text = "X-DSPAM-Confidence:    0.8475"
+search=text.find('0.8475')
+s=text[search: ]
+f=float(s)
+print(f)
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,16 @@
 # Files
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+s=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    text=line.find('0.')
+    t=line[text: ]
+    f=float(t)
+    s=s+f
+    count=count+1
+average=s/count
+print("Average spam confidence:", average)
 
-filename = "dataset/mbox-short.txt"
