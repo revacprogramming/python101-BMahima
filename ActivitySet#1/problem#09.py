0c5d0a7de2d20c03e857a77dc67190a14e355c93
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-18 15:04:34 +0000

    problem 8

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -13,3 +13,18 @@ for line in fh:
     
 lst.sort()
 print(lst)
+
+#lists problem 8.5
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh:
+    if not line.startswith("From "):
+        continue
+    s=line.split()
+    print(s[1])
+    count=count+1
+print("There were", count, "lines in the file with From as the first word")
