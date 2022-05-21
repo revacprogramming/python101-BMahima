ef13e3ef430e59a88b2c73f20c4f79e3c3791165
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-21 08:01:59 +0000

    dictionaries

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,4 +1,5 @@
 # List
+
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,20 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+d=dict()
+for line in handle:
+    if not line.startswith("From:"):
+        continue
+    s=line.split()
+    d[s[1]]=d.get(s[1],0)+1
+bigd=None
+bigword=None
+for word,count in d.items():
+    if bigd is None or count>bigd:
+        bigword=word
+        bigd=count
+print(bigword,bigd)
