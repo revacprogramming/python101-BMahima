10ebc50d70a605466ae2b7841dd3add4beace635
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-22 12:27:23 +0000

    tuples

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,19 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+count=0
+d=dict()
+for line in handle:
+    if not line.startswith("From "):
+        continue
+    s=line.split()
+    y=s[5].split(':')
+    z=y[0]
+    d[z]=d.get(z,0)+1
+for k,v in sorted(d.items()):
+    print(k,v)
+    
