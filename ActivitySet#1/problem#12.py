75ce7c2a9fd7f75b3d72da8e6510c41b30333def
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-25 14:48:54 +0000

    problem 12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,8 +1,29 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-filename=("dataset/regular_exp.txt")
+import re
+filename=("regg.txt")
 handle=open(filename)
+sum=0
+lst=list()
 for line in handle:
+    line=line.rstrip()
     y=re.findall('[0-9]+',line)
-    print(y)
-  
+    lst=lst+y
+for i in lst:
+    sum=sum+int(i)
+print(sum)
+
+
+'''import re
+filename=("regg.txt")
+handle=open(filename).read()
+sum=0
+y=re.findall('[0-9]+',handle)
+for i in y:
+  sum=sum+int(i)
+print(sum)'''
+    
+
+    
+
+  #if len(y)!=1: continue
