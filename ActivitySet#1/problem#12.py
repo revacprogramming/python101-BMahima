b84cf353260f926866437f4fcbb3eb553833d11f
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-05-25 03:57:59 +0000

    regular_exp

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,8 +1,8 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-filename="dataset/regular_exp"
-name="regular_exp"
-handle=open(name)
+filename=("dataset/regular_exp.txt")
+handle=open(filename)
 for line in handle:
-        
+    y=re.findall('[0-9]+',line)
+    print(y)
   
