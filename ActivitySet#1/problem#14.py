dca0d34c10fc656700c74e7472f4f645c77db838
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-06-02 17:55:29 +0000

    Network

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,5 +1,5 @@
  # Using Web Services
-# https://www.py4e.com/lessons/servces
+#https://www.py4e.com/lessons/servces
 # import urllib.request, urllib.parse, urllib.error
 # fhand=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1544659.html')
 # for line in fhand:
