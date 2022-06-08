e9643e1da388110bcad465d7e38e8c417a7a27ad
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-06-08 18:17:03 +0000

    14

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,4 +1,4 @@
- # Using Web Services
+# Using Web Services
 #https://www.py4e.com/lessons/servces
 # import urllib.request, urllib.parse, urllib.error
 #fhand=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1544659.html')
